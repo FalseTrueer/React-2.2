19620ad192cbbcab71f3428b408c718d1b95996c
Gleb <mega.kiselyov@mail.ru>
2025-01-15 20:04:33 +0300

    without jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
+import React from 'react';
 import logo from '/logo512.png';
 import './App.css';
 
-
-// Декларативный компонент App(его использование)
+// Декларативный компонент App
 export const App = () => {
-	// Сам App внутри содержит императивный стиль
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<h2> Year: {new Date().getFullYear()}</h2>
-			</header>
-		</div>
-	);
+// Императивный стиль начинается в return
+    return React.createElement(
+        'div',
+        { className: 'App' },
+        React.createElement(
+            'header',
+            { className: 'App-header' },
+            React.createElement('img', {
+                src: logo,
+                className: 'App-logo',
+                alt: 'logo'
+            }),
+            React.createElement(
+                'p',
+                null,
+                'Edit ',
+                React.createElement('code', null, 'src/App.js'),
+                ' and save to reload.'
+            ),
+            React.createElement(
+                'a',
+                {
+                    className: 'App-link',
+                    href: 'https://reactjs.org',
+                    target: '_blank',
+                    rel: 'noopener noreferrer'
+                },
+                'Learn React'
+            ),
+            React.createElement(
+                'h2',
+                null,
+                `Year: ${new Date().getFullYear()}`
+            )
+        )
+    );
 };
